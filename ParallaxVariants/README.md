56c7ee3ba885fff2f4d3fadb59b2c39b8f7b873c
Gaurav Mohanty <github.com/codetweek>
2023-04-18 09:13:41 +0530

    New Parallax Testing

---
+++ ParallaxVariants/README.md
@@ -0,0 +1,16 @@
+# Parallax Variants
+
+*Trying out various Parallax variants*
+
+### Instructions
+
+
+### Tech
+
+HTML, CSS, Javascript 
+
+![My Skills](https://skills.thijs.gg/icons?i=html,css,javascript,jquery&theme=dark)
+
+### Live
+
+<!-- [Color Format Convertor](https://codetweek.github.io/ColorFormatConvertor/src/) -->
---
+++ ParallaxVariants/src/js/script.js
@@ -0,0 +1,37 @@
+let mt1 = document.getElementsByClassName('par2')[0];
+let mt2 = document.getElementsByClassName('par3')[0];
+let mt3 = document.getElementsByClassName('par4')[0];
+let tr1 = document.getElementsByClassName('par5')[0];
+let tr2 = document.getElementsByClassName('par6')[0];
+let hero = document.getElementsByClassName('hero')[0];
+let txbg = document.getElementsByClassName('text-content')[0];
+
+let scene = document.querySelectorAll('.bg-par');
+
+window.addEventListener('scroll',()=> {
+
+    let value = window.scrollY;
+    mt1.style.top = value*0.5 + 'px';
+    mt2.style.top = value*0.4 + 'px';
+    mt3.style.top = value*0.3 + 'px';
+    tr1.style.top = value*0.2 + 'px';
+    tr2.style.top = value*0.1 + 'px';
+    hero.style.top = (value*0.6)+100 + 'px';
+    hero.style.textShadow = "0px 0px "+ (8-(value*0.05))+ "px rgba(255, 255, 255, 0.75)";
+    //.log(hero.style.textShadow);
+    mt1.style.left = value*0.0 + 'px';
+    mt2.style.left = value*0.1 + 'px';
+    mt3.style.left = value*-0.1 + 'px';
+    tr1.style.left = value*0.05 + 'px';
+
+    scene.forEach((item)=>{
+        item.style.filter="brightness(" +(1-(value*0.001))+ ")";
+        
+    });
+    // console.log((1-(value*0.001)));
+    // console.log((13-(13*(value*0.0008))));
+    txbg.style.backgroundColor="hsl(171, 76%, "+ (13-(13*(value*0.001))) + "%)";
+    
+
+
+});
